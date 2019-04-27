b742f0cb7299405e56a8e79cbe624a5b370e10c1
heliujie <268079@163.com>
2019-04-28 06:31:31 +0800

    routes

--- lib/demo/listview-demo.dart
+++ lib/demo/listview-demo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../model/post.dart';
+import './post_show.dart';
 
 class ListViewDemo extends StatelessWidget{
 
@@ -7,20 +8,40 @@ class ListViewDemo extends StatelessWidget{
     return Container(
       color: Colors.white,
       margin: EdgeInsets.all(8.0),
-      child: Column(
+      child: Stack(
         children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(height: 16.0),
-          Text(
-            posts[index].title,
-            style: Theme.of(context).textTheme.title
+          Column(
+            children: <Widget>[
+              Image.network(posts[index].imageUrl),
+              SizedBox(height: 16.0),
+              Text(
+                posts[index].title,
+                style: Theme.of(context).textTheme.title
+              ),
+              Text(
+                posts[index].author,
+                style: Theme.of(context).textTheme.subhead
+              ),
+            ],
           ),
-          Text(
-            posts[index].author,
-            style: Theme.of(context).textTheme.subhead
-          ),
+          Positioned.fill(
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                splashColor: Colors.white.withOpacity(0.3),
+                highlightColor: Colors.white.withOpacity(0.1),
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => PostShow(post: posts[index])
+                    )
+                  )
+                }
+              ),
+            ),
+          )
         ],
-      ),
+      )
     );
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import './demo/basec_demo.dart';
 import './demo/layout_demo.dart';
 import './demo/view_demo.dart';
 import './demo/sliver_demo.dart';
+import './demo/navigator_demo.dart';
 
 void main() => runApp(App());
 
@@ -13,7 +14,12 @@ class App extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      home:  SliverDemo(), // Home(),
+      // home:  // SliverDemo(), // Home(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(), //NavigatorDemo(),
+        '/about': (context) => Page(title: 'About')
+      },
       theme: ThemeData(
         primarySwatch: Colors.yellow,
         highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
